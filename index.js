4e2b50bf515dc6e37fb429a68ce909ea148a5d08
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-04 16:33:24 +0530

    make server

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+
 app.listen(7000);
