daac9a123ecb09fbc55c48e8f96cc7290fb9bf91
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-09 17:50:30 +0530

    learning  api's

---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require("./src/app")
+
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+})
---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+const notes = []
+
+//title , des
+//POST/notes
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+
+    res.status(201).json({
+        message: "Note created successfully"
+    })
+})
+
+app.get('/notes', (req, res) => {
+
+    res.status(200).json({
+        
+        message:"notes fatched successfully",
+        notes: notes
+
+    })
+})
+
+module.exports = app
