d5bb0aea4946a50cd32ee18b65753928857116b9
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-23 17:12:14 +0530

    patch method

--- src/app.js
+++ src/app.js
@@ -35,4 +35,17 @@ app.delete('/notes/:index', (req, res) => {
     })
 
 })
+
+//PATCH/notes/:index(1)
+app.patch('/notes/:index', (req, res) => {
+
+    const index = req.params.index // 1
+    const description = req.body.description
+
+    notes[index].description = description
+
+    res.status(200).json({
+        message: "Note updated successfully"
+    })
+})
 module.exports = app
